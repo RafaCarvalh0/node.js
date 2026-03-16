a9f2b04bd248147c372d83dfa9e9c0c4b18c55ca
RafaCarvalh0 <rafacarvalho601@gmail.com>
2026-03-16 19:11:19 -0300

    bugfix/rotas

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
+const cors = require('cors');
 const express = require("express")
-const rotaLivro = require("./routes/livro")
-const rotaFavorito = require("./routes/favorito")
-
-const cors = require("cors")
-
 const app = express()
-app.use(express.json())
-
-app.use('/livros', rotaLivro)
-app.use('/favoritos', rotaFavorito)
-
 const port = 8000
+
+app.use(cors());
+
+const livroRoutes = require('./routes/livro')
+const favoritoRoutes = require('./routes/favorito')
+
+app.use(express.json())
+app.use('/livros', livroRoutes)
+app.use('/favoritos', favoritoRoutes)
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
 })
